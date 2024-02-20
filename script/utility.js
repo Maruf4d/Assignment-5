7c0d75cd80fd503882ed3c76c42330f67d4645b5
Maruf4d <marufjava@gmail.com>
2024-02-20 11:45:39 +0600

    total Cost Function added

--- script/utility.js
+++ script/utility.js
@@ -1,13 +1,34 @@
-const allBtn = document.getElementsByClassName(`add-btn`);
+const allBtn = document.getElementsByClassName("add-btn");
 let count = + 0;
 
 for(const btn of allBtn){
     btn.addEventListener("click", function(e){
         count = count +1;
 
-        const seatPrice = e.target.parentNode.childNodes[1].innerText;
+        const seatSelected = e.target.parentNode.childNodes[1].innerText;
+        // console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[3].childNodes[5].childNodes[1].childNodes[3].innerText)
+        const seatPrice = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[3].childNodes[5].childNodes[1].childNodes[3].innerText;
+
+        const selectedContainer = document.getElementById("select-seat-container");
+        
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+        const p2 = document.createElement("p");
+        const p3 = document.createElement("h3");
+        
+        p.innerText =seatSelected;
+        p2.innerText = "economy";
+        p3.innerText = seatPrice;
+        li.appendChild(p)
+        li.appendChild(p2)
+        li.appendChild(p3)
+        selectedContainer.appendChild(li)
+
+        const totalCost = document.getElementById('total-price').innerText;
+        const convertedTotalPrice = parseInt(totalCost)
+        document.getElementById("total-price").innerText =convertedTotalPrice  + parseInt(seatPrice);
         
 
-        setInnerText(`card-count`, count)
+        setInnerText("card-count", count)
     })
 }

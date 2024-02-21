de6a79e046e98d2e04bdc665f81afb6bdf3f587b
Maruf4d <marufjava@gmail.com>
2024-02-21 22:40:25 +0600

    JavaScript Faunction added

--- script/style.js
+++ script/style.js
@@ -1,3 +1,46 @@
 function setInnerText(id, value){
     document.getElementById(id).innerText=value;
+    
+
 }
+
+
+function totalPrice(seatPrice){
+ 
+    const totalCost = getConvertValue("total-price");
+    const convertedTotalPrice = parseInt(seatPrice);
+    document.getElementById("total-price").innerText = totalCost +  convertedTotalPrice; 
+}
+
+function totalGrand(control){
+
+const totalCost = getConvertValue("total-price");
+    
+    if(control == undefined){
+        document.getElementById("grand-total").innerText = totalCost;
+    }
+
+    else{
+      const couponCode = document.getElementById("coupon-text").value;
+      if(couponCode == "Couple 20"){
+        const discount = totalCost * 0.2;
+        document.getElementById("grand-total").innerText = totalCost -discount;
+      }
+      else if(couponCode == "NEW15"){
+        const discount = totalCost * 0.15;
+        document.getElementById("grand-total").innerText = totalCost -discount;
+      }
+      else{
+        alert("vai please valid Coupon dew");
+      }
+    }
+}
+
+
+function getConvertValue(id){
+    const seatPrice = document.getElementById(id).innerText;
+    const seatConvert = parseInt(seatPrice);
+    return seatConvert;
+}
+
+
--- script/utility.js
+++ script/utility.js
@@ -1,3 +1,7 @@
+const ticketSection =  document.getElementById("scroll").addEventListener("click", function () {
+document.location = "#buy-ticket";
+});
+
 const allBtn = document.getElementsByClassName("add-btn");
 let count = + 0;
 
@@ -6,29 +10,40 @@ for(const btn of allBtn){
         count = count +1;
 
         const seatSelected = e.target.parentNode.childNodes[1].innerText;
-        // console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[3].childNodes[5].childNodes[1].childNodes[3].innerText)
         const seatPrice = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[3].childNodes[5].childNodes[1].childNodes[3].innerText;
-
         const selectedContainer = document.getElementById("select-seat-container");
-        
+
+        event.target.setAttribute("disabled", false);
         const li = document.createElement("li");
         const p = document.createElement("p");
         const p2 = document.createElement("p");
         const p3 = document.createElement("h3");
+
         
+        const seatLimit = getConvertValue("card-count");
+        if(seatLimit + 1 > 4){
+            alert("Over Limit");
+            return;
+        }
+
+        event.target.parentNode.style.backgroundColor = "gray";
+        const seatCount = getConvertValue(`seat-count`);
+        document.getElementById(`seat-count`).innerText = seatCount - 1;
+   
+
+
+
+
         p.innerText =seatSelected;
         p2.innerText = "economy";
         p3.innerText = seatPrice;
-        li.appendChild(p)
-        li.appendChild(p2)
-        li.appendChild(p3)
-        selectedContainer.appendChild(li)
+        li.appendChild(p);
+        li.appendChild(p2);
+        li.appendChild(p3);
+        selectedContainer.appendChild(li);
 
-        const totalCost = document.getElementById('total-price').innerText;
-        const convertedTotalPrice = parseInt(totalCost)
-        document.getElementById("total-price").innerText =convertedTotalPrice  + parseInt(seatPrice);
-        
-
-        setInnerText("card-count", count)
+        totalPrice(seatPrice);
+        totalGrand();
+        setInnerText("card-count",count);
     })
 }
